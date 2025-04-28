fa164a443aa292aa8d1829f0813ea4d74c3e36b3
soumia987 <soumas123456@gmail.com>
2025-04-28 17:27:47 +0100

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-red-600'>hello
+        
+        
+        
+      </div>
+      
+      
+    </>
+  )
+}
+
+export default App
